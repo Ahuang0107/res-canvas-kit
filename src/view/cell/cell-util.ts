fab7929da26fd50e9913183ee4d3efbc0989015e
Elase Huang <elase.huang@outlook.com>
2022-11-15 15:27:21 +0800

    🚀perf: remove text hover/focus style
    remove text hover/focus style to avoid lots of Paragraph create

--- src/view/cell/cell-util.ts
+++ src/view/cell/cell-util.ts
@@ -15,16 +15,16 @@ export type CellConfig = {
 type CellStyle = {
 	fillColor?: Color;
 	strokeColor?: Color;
-	textColor?: Color;
 };
 
+// todo 需要避免创建多余的ParaCache，所以hover和focus时尽量也使用同一个cache
 export function buildCell(frame: Rect, config: CellConfig): ComponentCaches {
 	const { text, textSize = 12, style, hoverStyle, focusStyle } = config;
 
 	let cache: DrawCache[] | undefined = undefined;
 	if (style) {
 		cache = [];
-		const { fillColor, strokeColor, textColor = CanvasKitUtil.CanvasKit.BLACK } = style;
+		const { fillColor, strokeColor } = style;
 
 		// todo 这里只是设置一个颜色的paint的话，不需要每次创建这个对象
 		if (fillColor) {
@@ -40,30 +40,12 @@ export function buildCell(frame: Rect, config: CellConfig): ComponentCaches {
 			strokePaint.setStrokeWidth(1);
 			cache.push(new RectCache(frame, strokePaint));
 		}
-		if (text) {
-			const paraStyle = new CanvasKitUtil.CanvasKit.ParagraphStyle({
-				textStyle: {
-					color: textColor,
-					fontSize: textSize
-				},
-				textAlign: CanvasKitUtil.CanvasKit.TextAlign.Center,
-				maxLines: 1,
-				ellipsis: '...'
-			});
-			cache.push(
-				new ParaCache(frame, text, paraStyle, new Point(0, (frame.height - textSize) / 2))
-			);
-		}
 	}
 
 	let hoverCache: DrawCache[] | undefined = undefined;
 	if (hoverStyle) {
 		hoverCache = [];
-		const {
-			fillColor: hoverFillColor,
-			strokeColor: hoverStrokeColor,
-			textColor: hoverTextColor
-		} = hoverStyle;
+		const { fillColor: hoverFillColor, strokeColor: hoverStrokeColor } = hoverStyle;
 		if (hoverFillColor) {
 			const fillPaint = new CanvasKitUtil.CanvasKit.Paint();
 			fillPaint.setColor(hoverFillColor);
@@ -77,32 +59,13 @@ export function buildCell(frame: Rect, config: CellConfig): ComponentCaches {
 			strokePaint.setStrokeWidth(1);
 			hoverCache.push(new RectCache(frame, strokePaint));
 		}
-
-		if (text) {
-			const paraStyle = new CanvasKitUtil.CanvasKit.ParagraphStyle({
-				textStyle: {
-					color: hoverTextColor,
-					fontSize: textSize
-				},
-				textAlign: CanvasKitUtil.CanvasKit.TextAlign.Center,
-				maxLines: 1,
-				ellipsis: '...'
-			});
-			hoverCache.push(
-				new ParaCache(frame, text, paraStyle, new Point(0, (frame.height - textSize) / 2))
-			);
-		}
 	}
 
 	let focusCache: DrawCache[] | undefined = undefined;
 
 	if (focusStyle) {
 		focusCache = [];
-		const {
-			fillColor: focusFillColor,
-			strokeColor: focusStrokeColor,
-			textColor: focusTextColor
-		} = focusStyle;
+		const { fillColor: focusFillColor, strokeColor: focusStrokeColor } = focusStyle;
 		if (focusFillColor) {
 			const fillPaint = new CanvasKitUtil.CanvasKit.Paint();
 			fillPaint.setColor(focusFillColor);
@@ -116,21 +79,28 @@ export function buildCell(frame: Rect, config: CellConfig): ComponentCaches {
 			strokePaint.setStrokeWidth(1);
 			focusCache.push(new RectCache(frame, strokePaint));
 		}
+	}
 
-		if (text) {
-			const paraStyle = new CanvasKitUtil.CanvasKit.ParagraphStyle({
-				textStyle: {
-					color: focusTextColor,
-					fontSize: textSize
-				},
-				textAlign: CanvasKitUtil.CanvasKit.TextAlign.Center,
-				maxLines: 1,
-				ellipsis: '...'
-			});
-			focusCache.push(
-				new ParaCache(frame, text, paraStyle, new Point(0, (frame.height - textSize) / 2))
-			);
-		}
+	if (text) {
+		const paraStyle = new CanvasKitUtil.CanvasKit.ParagraphStyle({
+			textStyle: {
+				color: CanvasKitUtil.CanvasKit.BLACK,
+				fontSize: textSize
+			},
+			textAlign: CanvasKitUtil.CanvasKit.TextAlign.Center,
+			maxLines: 1,
+			ellipsis: '...'
+		});
+		const paraCache = new ParaCache(
+			frame,
+			text,
+			paraStyle,
+			new Point(0, (frame.height - textSize) / 2),
+			true
+		);
+		cache?.push(paraCache);
+		hoverCache?.push(paraCache);
+		focusCache?.push(paraCache);
 	}
 
 	return {
